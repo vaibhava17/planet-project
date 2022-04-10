221bb42015629165c4f8fcf040d11570d376659c
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-04-10 12:39:04 +0530

    search done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const parse = require("csv-parse");
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
-console.log(parse);
+const habitablePlants = [];
+
+const isHabitablePlanets = planet => {
+      return planet["koi_disposition"] === "CONFIRMED"
+            && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
+            && planet["koi_prad"] < 1.6;
+}
+
+fs.createReadStream("kepler_data.csv")
+      .pipe(parse({
+            comment: "#",
+            columns: true,
+            // delimiter: ","
+      }))
+      .on("data", data => {
+            if (isHabitablePlanets(data)) {
+                  habitablePlants.push(data);
+            }
+      })
+      .on("error", err => {
+            console.log(err);
+      })
+      .on("end", () => {
+            console.log(`${habitablePlants.length} habitable planets found!`);
+            console.log(habitablePlants.map((planet) => {
+                  return `${planet["kepler_name"]} is a habitable planet!`
+            }));
+      });
